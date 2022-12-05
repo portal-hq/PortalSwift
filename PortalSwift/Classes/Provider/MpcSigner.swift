723035dfa5ad7da159f69bd1e58a1227e811c07c
kelsonic <kgadams93@gmail.com>
2022-12-05 15:32:53 -0500

    Add documentation to MPC Signer

--- PortalSwift/Classes/Provider/MpcSigner.swift
+++ PortalSwift/Classes/Provider/MpcSigner.swift
@@ -21,7 +21,6 @@ struct SignerResult: Codable {
 class MpcSigner {
   public var address: String?
   public var keychain: PortalKeychain
-
   private var mpcUrl: String
 
   init (
@@ -31,11 +30,17 @@ class MpcSigner {
     self.keychain = keychain
     self.mpcUrl = mpcUrl
   }
-
+  
+  /// Signs a standard ETH request.
+  /// - Parameters:
+  ///   - payload: A normal payload whose params are of type [Any].
+  ///   - provider: The provider is passed to MPC when signing.
+  /// - Returns: A SignerResult.
   public func sign(
     payload: ETHRequestPayload,
     provider: PortalProvider
   ) throws -> Any {
+    // Obtain the public address.
     let address = try keychain.getAddress()
 
     switch (payload.method) {
@@ -44,9 +49,9 @@ class MpcSigner {
     case ETHRequestMethods.Accounts.rawValue:
       return SignerResult(accounts: [address])
     default :
+      // Obtain the sign result.
       let signingShare = try keychain.getSigningShare()
       let formattedParams = try formatParams(payload: payload)
-      print("What are the params:", formattedParams)
       let clientSignResult = ClientSign(
         provider.getApiKey(),
         mpcUrl,
@@ -57,20 +62,28 @@ class MpcSigner {
         String(provider.chainId)
       )
 
+      // Attempt to decode the sign result.
       let jsonData = clientSignResult.data(using: .utf8)!
       let signResult: SignResult = try JSONDecoder().decode(SignResult.self, from: jsonData)
       guard signResult.error == "" else {
         throw MpcError.unexpectedErrorOnSign(message: signResult.error!)
       }
 
+      // Return the sign result.
       return SignerResult(signature: signResult.data!)
     }
   }
 
+  /// Signs a transaction request.
+  /// - Parameters:
+  ///   - payload: A payload whose params are of type [ETHTransactionParam].
+  ///   - provider: The provider is passed to MPC when signing.
+  /// - Returns: A SignerResult.
   public func sign(
     payload: ETHTransactionPayload,
     provider: PortalProvider
   ) throws -> SignerResult {
+    // Obtain the sign result.
     let signingShare = try keychain.getSigningShare()
     let formattedParams = try formatParams(payload: payload)
     let clientSignResult = ClientSign(
@@ -83,37 +96,33 @@ class MpcSigner {
       String(provider.chainId)
     )
 
+    // Attempt to decode the sign result.
     let jsonData = clientSignResult.data(using: .utf8)!
     let signResult: SignResult = try JSONDecoder().decode(SignResult.self, from: jsonData)
     guard signResult.error == "" else {
       throw MpcError.unexpectedErrorOnSign(message: signResult.error!)
     }
 
+    // Return the sign result.
     return SignerResult(signature: signResult.data!)
   }
 
   private func formatParams(payload: ETHRequestPayload) throws -> String {
-    var json: Data
-
     if payload.params.count == 0 {
       return ""
-    } else {
-      json = try JSONSerialization.data(withJSONObject: payload.params, options: .prettyPrinted)
     }
 
+    let json: Data = try JSONSerialization.data(withJSONObject: payload.params, options: .prettyPrinted)
     return String(data: json, encoding: .utf8)!
   }
 
   private func formatParams(payload: ETHTransactionPayload) throws -> String {
-    var json: Data
-
     if payload.params.count == 0 {
       return ""
-    } else {
-      let formattedPayload = payload.params.first!
-      json = try JSONEncoder().encode(formattedPayload)
     }
 
+    let formattedPayload = payload.params.first!
+    let json: Data = try JSONEncoder().encode(formattedPayload)
     return String(data: json, encoding: .utf8)!
   }
 }
