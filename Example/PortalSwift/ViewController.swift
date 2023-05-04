745aea2839f64bf51d37aafe70981b2e8586cf19
Blake Williams <blake@portalhq.io>
2023-05-04 11:12:56 -0700

    ENG-298 - Portal Connect (#39)
    * Start working out WebSocket connections
    
    * Support MobileStorageAdapter (#24)
    
    * Update file tree
    
    * Deprecating GG18 (#26)
    
    * changed default to v2
    
    * adding new framework
    
    * updating gitignore
    
    * updating to version 0.1.8
    
    * Release candidate (#31)
    
    * Webview signing on main thread (#28)
    
    * adding progress callbacks
    
    * adding all the progress args
    
    * adding gitingore
    
    * adding done to recover, adding env vars
    
    * reset parallel settings
    
    * reset test target
    
    * reset build script
    
    * remove print statements
    
    * fixing tests
    
    * testing main thread
    
    * adding background threads
    
    * fixed signing in webview, and formatted files
    
    * ENG-533 - Add completion handlers to setAddress and setSigningShare (#25)
    
    * Add completion handlers to setAddress and setSigningShare
    
    * Deprecating GG18 (#26)
    
    * changed default to v2
    
    * adding new framework
    
    * updating gitignore
    
    * updating to version 0.1.8
    
    * Webview signing on main thread (#28)
    
    * adding progress callbacks
    
    * adding all the progress args
    
    * adding gitingore
    
    * adding done to recover, adding env vars
    
    * reset parallel settings
    
    * reset test target
    
    * reset build script
    
    * remove print statements
    
    * fixing tests
    
    * testing main thread
    
    * adding background threads
    
    * fixed signing in webview, and formatted files
    
    * Remove repeat return
    
    ---------
    
    Co-authored-by: Rami Shahatit <rami@portalhq.io>
    
    * adding support for signTypedData and adding ENV var (#29)
    
    * v2->v3 (#30)
    
    * adding binary, fixing bugs
    
    * update injection script
    
    * bumping version
    
    ---------
    
    Co-authored-by: Blake Williams <blake@portalhq.io>
    
    * Start building out the WebSocket logic
    
    * Start building out PortalConnect
    
    * Add project updates
    
    * Add a bunch of types to get signing and connection working
    
    * Support MobileStorageAdapter (#24)
    
    * Update file tree
    
    * Release candidate (#31)
    
    * Webview signing on main thread (#28)
    
    * adding progress callbacks
    
    * adding all the progress args
    
    * adding gitingore
    
    * adding done to recover, adding env vars
    
    * reset parallel settings
    
    * reset test target
    
    * reset build script
    
    * remove print statements
    
    * fixing tests
    
    * testing main thread
    
    * adding background threads
    
    * fixed signing in webview, and formatted files
    
    * ENG-533 - Add completion handlers to setAddress and setSigningShare (#25)
    
    * Add completion handlers to setAddress and setSigningShare
    
    * Deprecating GG18 (#26)
    
    * changed default to v2
    
    * adding new framework
    
    * updating gitignore
    
    * updating to version 0.1.8
    
    * Webview signing on main thread (#28)
    
    * adding progress callbacks
    
    * adding all the progress args
    
    * adding gitingore
    
    * adding done to recover, adding env vars
    
    * reset parallel settings
    
    * reset test target
    
    * reset build script
    
    * remove print statements
    
    * fixing tests
    
    * testing main thread
    
    * adding background threads
    
    * fixed signing in webview, and formatted files
    
    * Remove repeat return
    
    ---------
    
    Co-authored-by: Rami Shahatit <rami@portalhq.io>
    
    * adding support for signTypedData and adding ENV var (#29)
    
    * v2->v3 (#30)
    
    * adding binary, fixing bugs
    
    * update injection script
    
    * bumping version
    
    ---------
    
    Co-authored-by: Blake Williams <blake@portalhq.io>
    
    * Update file system
    
    * Fix project structure
    
    * Fix build target
    
    ---------
    
    Co-authored-by: Rami Shahatit <rami@portalhq.io>

--- Example/PortalSwift/ViewController.swift
+++ Example/PortalSwift/ViewController.swift
@@ -47,15 +47,23 @@ class ViewController: UIViewController {
   @IBOutlet weak var ethBalanceInformation: UITextView!
 
   // Buttons
-  @IBOutlet public var generateButton: UIButton!
-  @IBOutlet public var backupButton: UIButton!
-  @IBOutlet public var recoverButton: UIButton!
-
+  @IBOutlet weak var backupButton: UIButton!
+  @IBOutlet weak var dappBrowserButton: UIButton!
+  @IBOutlet weak var generateButton: UIButton!
+  @IBOutlet weak var logoutButton: UIButton!
+  @IBOutlet weak var portalConnectButton: UIButton!
+  @IBOutlet weak var recoverButton: UIButton!
+  @IBOutlet weak var signInButton: UIButton!
+  @IBOutlet weak var signUpButton: UIButton!
+  @IBOutlet weak var testButton: UIButton!
+  
   // Send form
   @IBOutlet public var sendAddress: UITextField!
   @IBOutlet public var sendButton: UIButton!
   @IBOutlet public var username: UITextField!
   @IBOutlet public var url: UITextField!
+  
+  
   public var user: UserResult?
   public var CUSTODIAN_SERVER_URL: String?
   public var API_URL: String?
@@ -85,10 +93,28 @@ class ViewController: UIViewController {
       API_URL = STAGING_API_URL
       MPC_URL = STAGING_MPC_URL
     }
+    
+    DispatchQueue.main.async {
+      self.backupButton.isEnabled = false
+      self.dappBrowserButton.isEnabled = false
+      self.generateButton.isEnabled = false
+      self.logoutButton.isEnabled = false
+      self.portalConnectButton.isEnabled = false
+      self.recoverButton.isEnabled = false
+      self.signInButton.isEnabled = false
+      self.signUpButton.isEnabled = false
+      self.testButton.isEnabled = false
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let connectViewController = segue.destination as? ConnectViewController {
+      connectViewController.portal = self.portal
+    }
   }
 
   @IBAction func handleSignIn(_ sender: UIButton) {
@@ -97,6 +123,10 @@ class ViewController: UIViewController {
       self.user = userResult
       self.registerPortal(apiKey: userResult.clientApiKey)
       self.updateStaticContent()
+      
+      DispatchQueue.main.async {
+        self.logoutButton.isEnabled = true
+      }
     }
   }
 
@@ -106,6 +136,10 @@ class ViewController: UIViewController {
       self.user = userResult
       self.registerPortal(apiKey: userResult.clientApiKey)
       self.updateStaticContent()
+      
+      DispatchQueue.main.async {
+        self.logoutButton.isEnabled = true
+      }
     }
   }
 
@@ -113,6 +147,10 @@ class ViewController: UIViewController {
     self.user = nil
     self.addressInformation.text = "Address: N/A"
     self.ethBalanceInformation.text = "ETH Balance: N/A"
+    
+    DispatchQueue.main.async {
+      self.logoutButton.isEnabled = false
+    }
   }
 
   @IBAction func handleGenerate(_ sender: UIButton!) {
@@ -125,6 +163,14 @@ class ViewController: UIViewController {
       
       
       self.updateStaticContent()
+      
+      DispatchQueue.main.async {
+        self.backupButton.isEnabled = true
+        self.dappBrowserButton.isEnabled = true
+        self.portalConnectButton.isEnabled = true
+        self.recoverButton.isEnabled = true
+        self.testButton.isEnabled = true
+      }
     } progress: { status in
       print("Generate Status: ", status)
     }
@@ -205,6 +251,13 @@ class ViewController: UIViewController {
     handleSend()
   }
 
+  @IBAction func usernameChanged(_ sender: Any) {
+    let hasUsername = username.text?.count ?? 0 > 3
+    
+    signInButton.isEnabled = hasUsername
+    signUpButton.isEnabled = hasUsername
+  }
+  
   @IBAction func testProviderRequests(_ sender: UIButton!) {
     print("\n====================\nTesting provider methods\n====================")
     testSignerRequests()
@@ -357,6 +410,23 @@ class ViewController: UIViewController {
         apiHost: API_URL!,
         mpcHost: MPC_URL!
       )
+      
+      DispatchQueue.main.async {
+        do {
+          let address = try self.portal?.keychain.getAddress()
+          let hasAddress = address?.count ?? 0 > 0
+          
+          self.generateButton.isEnabled = true
+          
+          self.backupButton.isEnabled = hasAddress
+          self.dappBrowserButton.isEnabled = hasAddress
+          self.portalConnectButton.isEnabled = hasAddress
+          self.recoverButton.isEnabled = hasAddress
+          self.testButton.isEnabled = hasAddress
+        } catch {
+          print("Error fetching address: \(error)")
+        }
+      }
       
       _ = portal?.provider.on(event: Events.PortalSigningRequested.rawValue, callback: { [weak self] data in self?.didRequestApproval(data: data)})
     } catch ProviderInvalidArgumentError.invalidGatewayUrl {
