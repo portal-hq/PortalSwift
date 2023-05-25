db2e2d52f4ec03cb809939a253f53ab9455b7c40
Rami Shahatit <rami@portalhq.io>
2023-05-25 08:15:46 -0400

    testing merge of rc into blakes branch

--- Example/PortalSwift/ViewController.swift
+++ Example/PortalSwift/ViewController.swift
@@ -290,6 +290,9 @@ class ViewController: UIViewController {
   }
   func updateStaticContent() {
     populateAddressInformation()
+    retrieveNFTs()
+    getTransactions()
+    getBalances()
 //     populateEthBalance()
   }
 
@@ -312,6 +315,51 @@ class ViewController: UIViewController {
     let hexInt = Int(hexString, radix: 16)!
     let ethBalance = Double(hexInt) / 1000000000000000000
     return String(ethBalance)
+  }
+  
+  func retrieveNFTs() -> Void {
+    do {
+      try self.portal?.api.getNFTs() { (results) -> Void in
+        guard results.error == nil else {
+          print("❌ Unable to retrieve NFTs", results.error!)
+          return
+        }
+        
+        print("✅ Retrieved NFTs", results.data!)
+      }
+    } catch {
+      print("❌ Unable to retrieve NFTs", error)
+    }
+  }
+  
+  func getTransactions() -> Void {
+    do {
+      try self.portal?.api.getTransactions() { (results) -> Void in
+        guard results.error == nil else {
+          print("❌ Unable to get transactions", results.error!)
+          return
+        }
+        
+        print("✅ Retrieved transactions", results.data!)
+      }
+    } catch {
+      print("❌ Unable to retrieve transactions", error)
+    }
+  }
+  
+  func getBalances() -> Void {
+    do {
+      try self.portal?.api.getBalances() { (results) -> Void in
+        guard results.error == nil else {
+          print("❌ Unable to get balances", results.error!)
+          return
+        }
+        
+        print("✅ Retrieved balances", results.data!)
+      }
+    } catch {
+      print("❌ Unable to retrieve balances", error)
+    }
   }
 
   func populateEthBalance() {
