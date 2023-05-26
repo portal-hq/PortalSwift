d5249d7d407e38c551da4fcb165a491e427f0460
Rami Shahatit <rami@portalhq.io>
2023-05-25 23:50:40 -0400

    Automated E2E Tests (#73)
    Adding automated e2e tests

--- PortalSwift/Classes/Storage/Storage.swift
+++ PortalSwift/Classes/Storage/Storage.swift
@@ -12,25 +12,27 @@ public enum StorageError: Error {
 }
 
 /// Responsible for CRUD actions for items in the specified storage.
-public class Storage {
+open class Storage {
+  
+  public init() {}
   /// Deletes an item in storage.
   /// - Parameter completion: Resolves as a Result<Bool>.
   /// - Returns: Void
-  public func delete(completion: @escaping (Result<Bool>) -> Void) -> Void {
+  open func delete(completion: @escaping (Result<Bool>) -> Void) -> Void {
     completion(Result(error: StorageError.mustExtendStorageClass))
   }
 
   /// Reads an item from storage.
   /// - Parameter completion: Resolves as a Result<String>, which includes the value from storage for the specified key.
   /// - Returns: Void
-  public func read(completion: @escaping (Result<String>) -> Void) -> Void {
+  open func read(completion: @escaping (Result<String>) -> Void) -> Void {
     completion(Result(error: StorageError.mustExtendStorageClass))
   }
 
   /// Writes an item to storage.
   /// - Parameter completion: Resolves as a Result<Bool>.
   /// - Returns: Void
-  public func write(privateKey: String, completion: @escaping (Result<Bool>) -> Void) -> Void {
+  open func write(privateKey: String, completion: @escaping (Result<Bool>) -> Void) -> Void {
     completion(Result(error: StorageError.mustExtendStorageClass))
   }
 }
