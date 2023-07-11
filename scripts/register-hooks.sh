4006842223253f8948d89a64b6a8d8e0733fb9e3
Blake Williams <blakethewilliams@gmail.com>
2023-07-11 11:00:24 -0700

    Update register-hooks.sh

--- scripts/register-hooks.sh
+++ scripts/register-hooks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Installing `git-format-staged`..."
-npm install --global git-format-staged
+if [ -z "$(which git-format-staged)" ]; then
+  echo "Installing 'git-format-staged'..."
+  npm install --global git-format-staged
+fi
+
+if [ -z "$(which swiftformat)" ]; then
+  echo "Installing 'swiftformat'..."
+  brew install swiftformat
+fi
 
 echo "Moving to project root..."
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
