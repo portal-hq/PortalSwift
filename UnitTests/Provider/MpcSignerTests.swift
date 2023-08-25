f1a4b859d16a2da2245bb365a7a75178450e026b
Rami Shahatit <rami@portalhq.io>
2023-08-25 11:27:27 -0400

    mpc signer unit tests

--- UnitTests/Provider/MpcSignerTests.swift
+++ UnitTests/Provider/MpcSignerTests.swift
@@ -10,25 +10,22 @@
 import XCTest
 
 final class MpcSignerTests: XCTestCase {
-  var keychain: PortalKeychain?
-  var signer: MpcSigner?
-  var provider: PortalProvider?
+  var keychain: PortalKeychain!
+  var signer: MpcSigner!
+  var provider: PortalProvider!
 
   override func setUpWithError() throws {
-    do {
-      self.keychain = MockPortalKeychain()
-      self.keychain!.setSigningShare(signingShare: mockSigningShare) { _ in }
-      self.signer = MpcSigner(apiKey: "API_KEY", keychain: self.keychain!)
-      self.provider = try MockPortalProvider(
-        apiKey: "API_KEY",
-        chainId: Chains.Goerli.rawValue,
-        gatewayConfig: [Chains.Goerli.rawValue: "https://eth-goerli.g.alchemy.com/v2/API_KEY"],
-        keychain: MockPortalKeychain(),
-        autoApprove: true
-      )
-    } catch {
-      throw error
-    }
+    self.keychain = MockPortalKeychain()
+    self.keychain.setSigningShare(signingShare: mockSigningShare) { _ in }
+    self.signer = MpcSigner(apiKey: mockApiKey, keychain: self.keychain, binary: MockMobileWrapper())
+    self.provider = try PortalProvider(
+      apiKey: mockApiKey,
+      chainId: 5,
+      gatewayConfig: [5: mockHost],
+      keychain: MockPortalKeychain(),
+      autoApprove: true,
+      gateway: MockHttpRequester(baseUrl: mockHost)
+    )
   }
 
   override func tearDownWithError() throws {
@@ -37,24 +34,32 @@ final class MpcSignerTests: XCTestCase {
     self.provider = nil
   }
 
-  func testETHRequestPayloadSign() throws {
+  func testEthRequestAccounts() throws {
+    let payload = ETHRequestPayload(
+      method: ETHRequestMethods.RequestAccounts.rawValue,
+      params: []
+    )
+
+    let result: SignerResult = try signer.sign(payload: payload, provider: self.provider!)
+    let accounts = result.accounts
+    XCTAssert(accounts?.first == mockAddress)
+  }
+
+  func testEthAccounts() throws {
     let payload = ETHRequestPayload(
       method: ETHRequestMethods.Accounts.rawValue,
       params: []
     )
 
-    let result: SignerResult = try signer?.sign(payload: payload, provider: self.provider!) as! SignerResult
+    let result: SignerResult = try signer.sign(payload: payload, provider: self.provider!)
     let accounts = result.accounts
     XCTAssert(accounts?.first == mockAddress)
   }
 
-  func testETHTransactionPayloadSign() throws {
-    throw XCTSkip("Test is not implemented yet")
+  func testEthSendTransactionNoGas() throws {
     let fakeTransaction = ETHTransactionParam(
       from: mockAddress,
       to: "0x4cd042bba0da4b3f37ea36e8a2737dce2ed70db7",
-      gas: "0x76c0",
-      gasPrice: "0x9184e72a000",
       value: "0x9184e72a",
       data: "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
     )
@@ -64,7 +69,83 @@ final class MpcSignerTests: XCTestCase {
       params: [fakeTransaction]
     )
 
-    let result = try signer?.sign(payload: payload, provider: self.provider!, mockClientSign: true)
-    XCTAssert(result?.signature == mockSignature)
+    let result = try signer.sign(payload: payload, provider: self.provider)
+    XCTAssert(result.signature == mockSignature)
+  }
+
+  func testEthSendTransactionWithGas() throws {
+    let fakeTransaction = ETHTransactionParam(
+      from: mockAddress,
+      to: "0x4cd042bba0da4b3f37ea36e8a2737dce2ed70db7",
+      gas: "0x9184e72a",
+      gasPrice: "0x76c0",
+      value: "0x9184e72a000",
+      data: "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
+    )
+
+    let payload = ETHTransactionPayload(
+      method: ETHRequestMethods.SendTransaction.rawValue,
+      params: [fakeTransaction]
+    )
+
+    let result = try signer.sign(payload: payload, provider: self.provider)
+    XCTAssert(result.signature == mockSignature)
+  }
+
+  func testEthSignWithParams() throws {
+    let payload = ETHRequestPayload(
+      method: ETHRequestMethods.Sign.rawValue, params: [mockAddress, "0xdeadbeaf"]
+    )
+
+    let result = try signer.sign(payload: payload, provider: self.provider)
+    XCTAssert(result.signature == mockSignature)
+  }
+
+  func testEthSignWithNoParams() throws {
+    let payload = ETHRequestPayload(
+      method: ETHRequestMethods.Sign.rawValue, params: []
+    )
+
+    XCTAssertThrowsError(try self.signer.sign(payload: payload, provider: self.provider))
+  }
+
+  func testEthSendTransactionWithNoParams() throws {
+    let payload = ETHTransactionPayload(
+      method: ETHRequestMethods.SendTransaction.rawValue,
+      params: []
+    )
+    XCTAssertThrowsError(try self.signer.sign(payload: payload, provider: self.provider))
+  }
+
+  func testEthSignWithParamsError() throws {
+    // Override the signer with a instance of the binary that returns an error object.
+    self.signer = MpcSigner(apiKey: mockApiKey, keychain: self.keychain, binary: MockMobileErrorWrapper())
+
+    let payload = ETHRequestPayload(
+      method: ETHRequestMethods.Sign.rawValue, params: [mockAddress, "0xdeadbeaf"]
+    )
+
+    XCTAssertThrowsError(try self.signer.sign(payload: payload, provider: self.provider))
+  }
+
+  func testEthSendTransactionWithGasError() throws {
+    // Override the signer with a instance of the binary that returns an error object.
+    self.signer = MpcSigner(apiKey: mockApiKey, keychain: self.keychain, binary: MockMobileErrorWrapper())
+
+    let fakeTransaction = ETHTransactionParam(
+      from: mockAddress,
+      to: "0x4cd042bba0da4b3f37ea36e8a2737dce2ed70db7",
+      gas: "0x9184e72a",
+      gasPrice: "0x76c0",
+      value: "0x9184e72a000",
+      data: "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
+    )
+
+    let payload = ETHTransactionPayload(
+      method: ETHRequestMethods.SendTransaction.rawValue,
+      params: [fakeTransaction]
+    )
+
+    XCTAssertThrowsError(try self.signer.sign(payload: payload, provider: self.provider))
   }
 }
