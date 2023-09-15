cade15389d4af299786cd695b96d091620deae2d
kelsonic <kgadams93@gmail.com>
2023-09-15 19:00:13 +0200

    Add testZSign

--- Example/Tests/e2e/WalletTests.swift
+++ Example/Tests/e2e/WalletTests.swift
@@ -210,4 +210,46 @@ class WalletTests: XCTestCase {
     }
     wait(for: [recoverExpectation], timeout: 120)
   }
+  
+  func testZSign() {
+    if !WalletTests.testAGenerateSucceeded {
+      XCTFail("Failing fast - Generate test failed to complete successfully")
+      return
+    }
+
+    let ethSignExpectation = XCTestExpectation(description: "eth sign")
+    var address: String? = "0x290766b47d6ea98bae2bd189cc8c7b4aa3154371"
+
+    self.testLogin { result in
+      guard result.error == nil else {
+        ethSignExpectation.fulfill()
+        return XCTFail("Failed on login: \(result.error!)")
+      }
+      if let portal = WalletTests.PortalWrap.portal {
+        do {
+          address = try portal.keychain.getAddress()
+        } catch {
+          return XCTFail("Failed to get address: \(error)")
+        }
+
+        let params = [address!, "0xdeadbeaf"]
+
+        WalletTests.PortalWrap.ethSign(params: params) { result in
+          guard result.error == nil else {
+            ethSignExpectation.fulfill()
+            return XCTFail("Failed on eth_sign: \(result.error!)")
+          }
+
+          print("✅ eth_sign result: ", result.data!)
+          XCTAssertFalse(result.data!.isEmpty, "eth sign success")
+
+          ethSignExpectation.fulfill()
+        }
+      } else {
+        return XCTFail("Failed to register portal object.")
+      }
+    }
+
+    wait(for: [ethSignExpectation], timeout: 30)
+  }
 }
