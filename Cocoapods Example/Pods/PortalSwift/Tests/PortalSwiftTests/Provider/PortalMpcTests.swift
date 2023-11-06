f1bf4cb74866d46af866002d0b70f02bcb409079
Blake Williams <blake@portalhq.io>
2023-11-06 13:31:14 -0800

    Release version 2.0.11 (#193)
    * Kelsonic/eng 1421 remove all force unwrapping (#176)
    
    * Add swiftlint
    
    * Fix force casting
    
    * Fix force casting partially in PortalMpc
    
    * Resolve more linting errors
    
    * Fix force casting and force unwrapping
    
    * Fix force casting and force unwrapping
    
    * Remove swiftlint from pre commit hook
    
    * Fix linting errors
    
    * Add guard clause
    
    * ENG-1611 - Make `requiredNamespaces` fields optional (#178)
    
    * Make `requiredNamespaces` fields optional
    
    * Fix type issues in `PortalConnect`
    
    * Fix default values in `addChainsToProposal`
    
    * Password backup (#179)
    
    * adding password backup method
    * correct binary
    * remove force unwrapping
    * [create-pull-request] automated change
    * fixing test
    ---------
    
    Co-authored-by: portal-release-bot <release-bot@portalhq.io>
    
    * Add unbindClientEvents method (#182)
    
    * Kelsonic/eng 1624 gettransactions should support order query param chainId (#183)
    
    * Add order + custom chainId support for getTransactions
    
    * Fix linting
    
    * Add support for `allowanceTarget` in the Swaps Quote response (#184)
    
    * Enable websiteDataStore on MKWebView configuration (#187)
    
    * Add permissions check to `validateOperations` for GDrive (#188)
    
    * Revert "Add permissions check to `validateOperations` for GDrive (#188)" (#191)
    
    This reverts commit 23b69bf45cd8394ece4d2380c4726177303b1e15.
    
    * Kelsonic/eng 1588 v5 (#185)
    
    * [create-pull-request] automated change
    
    * Update struct
    
    * Bump to v5 as default
    
    * Update storedClientSigningShare func to use v2 endpoint
    
    * Get tests to pass
    
    ---------
    
    Co-authored-by: portal-release-bot <release-bot@portalhq.io>
    
    * ENG-1707 - Add SPM Support to existing Workspace (#189)
    
    * Start getting builds working
    
    * Start getting builds working
    
    * Fix local builds
    
    * Re-add SPM Example
    
    * Fix unit tests
    
    * Rename unit tests
    
    * Fix unit tests
    
    * Remove build directory
    
    * Ignore .build directory
    
    * Remove swiftpm directory
    
    * Ignore swiftpm
    
    * REmove SPM Example files
    
    * Fix Github Action
    
    * Update Unit Tests Github Action
    
    * Fix unit tests workspace config
    
    * Update Test plan
    
    * cd into main to run Unit Tests
    
    * Capitalize `Mpc.xcframework`
    
    * Use a test plan
    
    * Remove testplan from xcodebuild
    
    * Get SPM building
    
    * Get SPM Building
    
    * Put tests back on one line
    
    * Get SPM building with E2E tests
    
    * Update readme
    
    * Add comment to README
    
    * Migrate 2.0.10 changes
    
    * Update binary
    
    * Fix conflic with provisionWallet
    
    * Remove duplicate tests
    
    * Fix testing plan for Unit Tests
    
    * Fix unit tests
    
    * Fix testing plan
    
    * Test in parallel
    
    * Don't test in parallel afterall
    
    * Update Codecov filters
    
    * Exclude Pods as well
    
    * Delete pods and remove Package.resolved
    
    * Remove Package.resolved & Pods
    
    * Update release script
    
    * Run unit tests
    
    * Add back in maintenance docs and pre-commit hooks
    
    * Add signing profiles
    
    * Fix unit tests
    
    ---------
    
    Co-authored-by: Kelson Adams <kelsonic@users.noreply.github.com>
    Co-authored-by: Rami Shahatit <rami@portalhq.io>
    Co-authored-by: portal-release-bot <release-bot@portalhq.io>

---
+++ Cocoapods Example/Pods/PortalSwift/Tests/PortalSwiftTests/Provider/PortalMpcTests.swift
@@ -0,0 +1,166 @@
+//
+//  PortalMpcTests.swift
+//  PortalSwift_Tests
+//
+//  Created by Portal Labs, Inc.
+//  Copyright © 2022 Portal Labs, Inc. All rights reserved.
+//
+
+@testable import PortalSwift
+import XCTest
+
+final class PortalMpcTests: XCTestCase {
+  var mpc: PortalMpc!
+  var provider: PortalProvider!
+
+  override func setUpWithError() throws {
+    let keychain = MockPortalKeychain()
+    keychain.clientId = mockClientId
+
+    self.provider = try MockPortalProvider(
+      apiKey: mockApiKey,
+      chainId: 5,
+      gatewayConfig: [5: mockHost],
+      keychain: keychain,
+      autoApprove: true
+    )
+
+    self.mpc = PortalMpc(
+      apiKey: mockApiKey,
+      api: MockPortalApi(
+        apiKey: mockApiKey,
+        apiHost: mockHost,
+        provider: self.provider,
+        mockRequests: true
+      ),
+      keychain: keychain,
+      storage: BackupOptions(icloud: MockICloudStorage()),
+      mobile: MockMobileWrapper()
+    )
+  }
+
+  override func tearDownWithError() throws {
+    self.mpc = nil
+  }
+
+  func testBackupIcloud() throws {
+    let expectation = XCTestExpectation(description: "Backup")
+    var encounteredStatuses: Set<MpcStatuses> = []
+
+    self.mpc.backup(method: BackupMethods.iCloud.rawValue) { result in
+      guard result.error == nil else {
+        XCTFail("Failure: \(String(describing: result.error))")
+        expectation.fulfill()
+        return
+      }
+
+      XCTAssert(result.data! as String == mockCiphertext, "Backup should return cipherText")
+      expectation.fulfill()
+    } progress: { MpcStatus in
+      let status = MpcStatus.status
+      encounteredStatuses.insert(status)
+    }
+
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertEqual(encounteredStatuses, backupProgressCallbacks, "All expected statuses should be encountered")
+  }
+
+  func testBackupGdrive() throws {
+    self.mpc = PortalMpc(
+      apiKey: mockApiKey,
+      api: MockPortalApi(
+        apiKey: mockApiKey,
+        apiHost: mockHost,
+        provider: self.provider,
+        mockRequests: true
+      ),
+      keychain: MockPortalKeychain(),
+      storage: BackupOptions(gdrive: MockGDriveStorage(clientID: mockClientId, viewController: UIViewController())),
+      mobile: MockMobileWrapper()
+    )
+    let expectation = XCTestExpectation(description: "Backup")
+    var encounteredStatuses: Set<MpcStatuses> = []
+
+    self.mpc.backup(method: BackupMethods.GoogleDrive.rawValue) { result in
+      guard result.error == nil else {
+        XCTFail("Failure: \(String(describing: result.error))")
+        expectation.fulfill()
+        return
+      }
+
+      XCTAssert(result.data! as String == mockCiphertext, "Backup should return cipherText")
+      expectation.fulfill()
+    } progress: { MpcStatus in
+      let status = MpcStatus.status
+      encounteredStatuses.insert(status)
+    }
+
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertEqual(encounteredStatuses, backupProgressCallbacks, "All expected statuses should be encountered")
+  }
+
+  func testGenerate() throws {
+    let expectation = XCTestExpectation(description: "Generate")
+
+    var encounteredStatuses: Set<MpcStatuses> = []
+
+    self.mpc.generate { addressResult in
+
+      guard addressResult.error == nil else {
+        XCTFail("Failure: \(String(describing: addressResult.error))")
+        expectation.fulfill()
+        return
+      }
+
+      XCTAssert(addressResult.data == mockAddress)
+      expectation.fulfill()
+    } progress: { MpcStatus in
+      let status = MpcStatus.status
+      encounteredStatuses.insert(status)
+    }
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertEqual(encounteredStatuses, generateProgressCallbacks, "All expected statuses should be encountered")
+  }
+
+  func testRecover() throws {
+    let expectation = XCTestExpectation(description: "Recover")
+    var encounteredStatuses: [MpcStatuses] = []
+
+    self.mpc.recover(cipherText: mockCiphertext, method: BackupMethods.iCloud.rawValue) { result in
+      guard result.error == nil else {
+        XCTFail("Failure: \(String(describing: result.error))")
+        expectation.fulfill()
+        return
+      }
+      XCTAssert(result.data! as String == mockAddress, "Recover should return address")
+      expectation.fulfill()
+    } progress: { MpcStatus in
+      let status = MpcStatus.status
+      encounteredStatuses.append(status)
+    }
+    wait(for: [expectation], timeout: 5.0)
+    print(encounteredStatuses)
+    XCTAssertEqual(encounteredStatuses, recoverProgressCallbacks, "All expected statuses should be encountered")
+  }
+
+  func testLegacyRecover() throws {
+    let expectation = XCTestExpectation(description: "Legacy Recover")
+    var encounteredStatuses: [MpcStatuses] = []
+
+    self.mpc.legacyRecover(cipherText: mockCiphertext, method: BackupMethods.iCloud.rawValue) { result in
+      guard result.error == nil else {
+        XCTFail("Failure: \(String(describing: result.error))")
+        expectation.fulfill()
+        return
+      }
+      XCTAssert(result.data! as String == mockCiphertext, "Recover should return cipherText")
+      expectation.fulfill()
+    } progress: { MpcStatus in
+      let status = MpcStatus.status
+      encounteredStatuses.append(status)
+    }
+    wait(for: [expectation], timeout: 5.0)
+    print(encounteredStatuses)
+    XCTAssertEqual(encounteredStatuses, legacyRecoverProgressCallbacks, "All expected statuses should be encountered")
+  }
+}
---
+++ Cocoapods Example/Pods/PortalSwift/Tests/PortalSwiftTests/Storage/GDriveStorageTests.swift
@@ -0,0 +1,16 @@
+//
+//  GDriveStorageTests.swift
+//  PortalSwift_Tests
+//
+//  Created by Portal Labs, Inc.
+//  Copyright © 2022 Portal Labs, Inc. All rights reserved.
+//
+
+@testable import PortalSwift
+import XCTest
+
+final class GDriveStorageTests: XCTestCase {
+  func testExample() {
+    XCTAssertTrue(true)
+  }
+}
