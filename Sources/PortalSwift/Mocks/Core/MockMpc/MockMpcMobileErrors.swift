61c2cab7f0ced31b6c6efc1f2f2584d0bc55e07f
parsaattari <attari.parsa@gmail.com>
2024-01-19 17:06:46 -0500

    Revert "Parsa/eject tests"

--- Sources/PortalSwift/Mocks/Core/MockMpc/MockMpcMobileErrors.swift
+++ Sources/PortalSwift/Mocks/Core/MockMpc/MockMpcMobileErrors.swift
@@ -52,8 +52,4 @@ class MockMobileErrorWrapper: Mobile {
   func MobileSign(_: String?, _: String?, _: String?, _: String?, _: String?, _: String?, _: String?, _: String?) -> String {
     return mockClientSignResultWithError
   }
-    
-    func MobileEjectWalletAndDiscontinueMPC(_: String, _: String) -> String {
-        return mockEjectWalletAndDiscontinueMPC
-    }
 }
