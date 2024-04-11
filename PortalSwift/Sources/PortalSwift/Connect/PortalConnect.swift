aa002427629e4e275a4c651cfc6563304cfdf0ef
kelsonic <kgadams93@gmail.com>
2024-04-11 21:03:47 +0200

    Move PortalConnect into new pkg, update chain management logic

--- PortalSwift/Sources/PortalSwift/Connect/PortalConnect.swift
+++ PortalSwift/Sources/PortalSwift/Connect/PortalConnect.swift
@@ -22,10 +22,9 @@ public class PortalConnect: EventBus {
     return self.provider.address
   }
 
-  public var chainId: Int {
-    return self.provider.chainId
-  }
+  public var chainId: Int
 
+  @available(*, deprecated, renamed: "chainId", message: "Please use the public var on the PortalConnect instance.")
   public func setChainId(value: Int) throws {
     _ = try self.provider.setChainId(value: value, connect: self)
   }
@@ -38,16 +37,17 @@ public class PortalConnect: EventBus {
   public var uri: String?
 
   private let apiKey: String
-  private let webSocketServer: String
+  private let logger = PortalLogger()
   private let provider: PortalProvider
+  private var rpcConfig: [String: String]
   private var topic: String?
-  private var gatewayConfig: [Int: String]
+  private let webSocketServer: String
 
   public init(
     _ apiKey: String,
     _ chainId: Int,
     _ keychain: PortalKeychain,
-    _ gatewayConfig: [Int: String],
+    _ rpcConfig: [String: String],
     _ webSocketServer: String = "connect.portalhq.io",
     _ autoApprove: Bool = false,
     _ apiHost: String = "api.portalhq.io",
@@ -55,13 +55,14 @@ public class PortalConnect: EventBus {
     _ version: String = "v6"
   ) throws {
     self.apiKey = apiKey
+    self.chainId = chainId
     self.webSocketServer = webSocketServer
-    self.gatewayConfig = gatewayConfig
+    self.rpcConfig = rpcConfig
+
     // Initialize the PortalProvider
     self.provider = try PortalProvider(
       apiKey: apiKey,
-      chainId: chainId,
-      gatewayConfig: gatewayConfig,
+      rpcConfig: rpcConfig,
       keychain: keychain,
       autoApprove: autoApprove,
       apiHost: apiHost,
@@ -117,29 +118,67 @@ public class PortalConnect: EventBus {
     }
   }
 
+  @available(*, deprecated, renamed: "createPortalConnectInstance", message: "Please use portal.createPortalConnectInstance().")
+  public convenience init(
+    _ apiKey: String,
+    _ chainId: Int,
+    _ keychain: PortalKeychain,
+    _ gatewayConfig: [Int: String],
+    _ webSocketServer: String = "connect.portalhq.io",
+    _ autoApprove: Bool = false,
+    _ apiHost: String = "api.portalhq.io",
+    _ mpcHost: String = "mpc.portalhq.io",
+    _ version: String = "v6"
+  ) throws {
+    //  Handle the legacy use case of using Ethereum references as keys
+    let rpcConfig: [String: String] = Dictionary(gatewayConfig.map { key, value in
+      let newKey = "eip155:\(key)"
+      return (newKey, value)
+    }, uniquingKeysWith: { first, _ in first })
+
+    try self.init(
+      apiKey,
+      chainId,
+      keychain,
+      rpcConfig,
+      webSocketServer,
+      autoApprove,
+      apiHost,
+      mpcHost,
+      version
+    )
+  }
+
   deinit {
     client?.sendFinalMessageAndDisconnect()
   }
 
   public func connect(_ uri: String) {
-    if self.connected && uri == self.uri {
-      print("[PortalConnect] Connection is already in progress or established. Ignoring request to connect...")
-      return
+    do {
+      self.logger.info("⚠️ PortalConnect.connect() - Trying to connect.")
+      if self.connected && uri == self.uri {
+        self.logger.info("PortalConnect.connect() - Connection is already in progress or established. Ignoring request to connect.")
+        return
+      }
+      if self.client == nil {
+        self.client = WebSocketClient(apiKey: self.apiKey, connect: self, webSocketServer: self.webSocketServer)
+      } else {
+        self.unbindClientEvents()
+      }
+      guard let client = self.client else {
+        throw PortalConnectError.noWebSocketClientFound
+      }
+
+      self.uri = uri
+      client.resetEventBus()
+
+      self.bindClientEvents()
+
+      self.logger.info("⚠️ PortalConnect.connect() - Invoking client.connect()")
+      client.connect(uri: uri)
+    } catch {
+      self.logger.error("⚠️ PortalConnect.connect() - Unable to connect: \(error.localizedDescription)")
     }
-
-    if self.client == nil {
-      self.client = WebSocketClient(apiKey: self.apiKey, connect: self, webSocketServer: self.webSocketServer)
-    } else {
-      self.unbindClientEvents()
-    }
-
-    self.uri = uri
-
-    self.client?.resetEventBus()
-
-    self.bindClientEvents()
-
-    self.client?.connect(uri: uri)
   }
 
   public func disconnect(_ userInitiated: Bool = false) {
@@ -147,7 +186,7 @@ public class PortalConnect: EventBus {
   }
 
   public func addChainsToProposal(data: ConnectData) -> ConnectData {
-    let chainsToAdd = self.gatewayConfig.keys.map { "eip155:\($0)" }
+    let chainsToAdd = self.rpcConfig.keys
 
     // Convert existing chains and chainsToAdd to sets
     let existingChainsSet = Set<String>(data.params.params.requiredNamespaces.eip155?.chains ?? [])
@@ -198,7 +237,7 @@ public class PortalConnect: EventBus {
           id: connectData.id,
           topic: connectData.topic,
           address: self.address!,
-          chainId: String(self.chainId),
+          chainId: String(self.chainId ?? 11_155_111),
           params: connectData.params
         )
       )
@@ -227,7 +266,7 @@ public class PortalConnect: EventBus {
           id: connectData.id,
           topic: connectData.topic,
           address: self.address!,
-          chainId: String(self.chainId),
+          chainId: String(self.chainId ?? 11_155_111),
           params: connectData.params
         )
       )
@@ -270,194 +309,185 @@ public class PortalConnect: EventBus {
   }
 
   func handleConnectError(data: ConnectError) {
-    var errorData = ErrorData(id: "0", topic: self.topic ?? "0", params: data)
+    let errorData = ErrorData(id: "0", topic: self.topic ?? "0", params: data)
     emit(event: Events.ConnectError.rawValue, data: errorData)
   }
 
   func handleSessionRequest(data: SessionRequestData) {
-    let (id, method, params, topic, chainId) = (
-      data.id,
-      data.params.request.method,
-      data.params.request.params,
-      data.topic,
-      data.params.chainId
-    )
-
-    on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
-      guard let self = self else { return }
-
-      let event = SignatureReceivedMessage(
-        event: "portal_signatureRejected",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: "",
-          transactionId: id
-        )
-      )
-
+    Task {
       do {
-        let message = try JSONEncoder().encode(event)
-        self.client?.send(message)
-      } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
-      }
-    }
-
-    let newChainId = chainId ?? self.chainId // use the default chain when there is no chainId sent from Wallet Connect
-    self.handleProviderRequest(method: method, params: params, chainId: newChainId) { [weak self] result in
-      guard let self = self else { return }
-
-      if result.error != nil {
-        print("[PortalConnect] \(result.error!)")
-        return
-      }
-
-      let signature = ((result.data as! RequestCompletionResult).result as! Result<SignerResult>).data!.signature!
-
-      let event = SignatureReceivedMessage(
-        event: "signatureReceived",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: signature,
-          transactionId: id
+        let (id, method, params, topic, chainId) = (
+          data.id,
+          data.params.request.method,
+          data.params.request.params,
+          data.topic,
+          data.params.chainId
         )
-      )
 
-      do {
+        on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
+          guard let self = self else { return }
+          let event = SignatureReceivedMessage(
+            event: "portal_signatureRejected",
+            data: SignatureReceivedData(
+              topic: topic,
+              transactionHash: "",
+              transactionId: id
+            )
+          )
+          do {
+            let message = try JSONEncoder().encode(event)
+            self.client?.send(message)
+          } catch {
+            self.logger.error("⚠️ PortalConnect.on(portal_signingRejected) -  Error encoding SignatureReceivedMessage: \(error)")
+          }
+        }
+
+        // use the default chain when there is no chainId sent from Wallet Connect
+        let newChainId = chainId ?? self.chainId ?? 11_155_111
+        let result = try await self.handleProviderRequest(method: method, params: params, chainId: newChainId)
+
+        guard let signature = result as? String else {
+          throw PortalConnectError.unableToParseSignResponse
+        }
+
+        let event = SignatureReceivedMessage(
+          event: "signatureReceived",
+          data: SignatureReceivedData(
+            topic: topic,
+            transactionHash: signature,
+            transactionId: id
+          )
+        )
+
         let message = try JSONEncoder().encode(event)
         self.client?.send(message)
 
         // emit the PortalSignatureReceived event on the PortalConnect EventBus as a convenience
         if signMethods.contains(method) {
-          self.emit(event: Events.PortalSignatureReceived.rawValue, data: result.data!)
+          self.emit(event: Events.PortalSignatureReceived.rawValue, data: signature)
         }
       } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
+        self.logger.error("⚠️ Error processing session request: \(error)")
       }
     }
   }
 
   func handleSessionRequestAddress(data: SessionRequestAddressData) {
-    let (id, method, params, topic, chainId) = (
-      data.id,
-      data.params.request.method,
-      data.params.request.params,
-      data.topic,
-      data.params.chainId
-    )
-
-    on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
-      guard let self = self else { return }
-
-      let event = SignatureReceivedMessage(
-        event: "portal_signatureRejected",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: "",
-          transactionId: id
-        )
-      )
-
+    Task {
       do {
-        let message = try JSONEncoder().encode(event)
-        self.client?.send(message)
-      } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
-      }
-    }
-
-    let newChainId = chainId ?? self.chainId // use the default chain when there is no chainId sent from Wallet Connect
-    self.handleProviderRequest(method: method, params: params, chainId: newChainId) { [weak self] result in
-      guard let self = self else { return }
-
-      if result.error != nil {
-        print("[PortalConnect] \(result.error!)")
-        return
-      }
-
-      let signature = ((result.data as! RequestCompletionResult).result as! Result<SignerResult>).data!.signature!
-      let event = SignatureReceivedMessage(
-        event: "signatureReceived",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: signature,
-          transactionId: id
+        let (id, method, params, topic, chainId) = (
+          data.id,
+          data.params.request.method,
+          data.params.request.params,
+          data.topic,
+          data.params.chainId
         )
-      )
 
-      do {
+        on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
+          guard let self = self else { return }
+          let event = SignatureReceivedMessage(
+            event: "portal_signatureRejected",
+            data: SignatureReceivedData(
+              topic: topic,
+              transactionHash: "",
+              transactionId: id
+            )
+          )
+          do {
+            let message = try JSONEncoder().encode(event)
+            self.client?.send(message)
+          } catch {
+            self.logger.error("⚠️ PortalConnect.on(portal_signingRejected) -  Error encoding SignatureReceivedMessage: \(error)")
+          }
+        }
+
+        // use the default chain when there is no chainId sent from Wallet Connect
+        let newChainId = chainId ?? self.chainId ?? 11_155_111
+        let result = try await self.handleProviderRequest(method: method, params: params, chainId: newChainId)
+
+        guard let signature = result as? String else {
+          throw PortalConnectError.unableToParseSignResponse
+        }
+
+        let event = SignatureReceivedMessage(
+          event: "signatureReceived",
+          data: SignatureReceivedData(
+            topic: topic,
+            transactionHash: signature,
+            transactionId: id
+          )
+        )
+
         let message = try JSONEncoder().encode(event)
         self.client?.send(message)
 
         // emit the PortalSignatureReceived event on the PortalConnect EventBus as a convenience
         if signMethods.contains(method) {
-          self.emit(event: Events.PortalSignatureReceived.rawValue, data: result.data!)
+//          self.emit(event: Events.PortalSignatureReceived.rawValue, data: result.data!)
         }
       } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
+        self.logger.error("⚠️ Error processing session request: \(error)")
       }
     }
   }
 
   func handleSessionRequestTransaction(data: SessionRequestTransactionData) {
-    let (id, method, params, topic, chainId) = (
-      data.id,
-      data.params.request.method,
-      data.params.request.params,
-      data.topic,
-      data.params.chainId
-    )
-
-    on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
-      guard let self = self else { return }
-
-      let event = SignatureReceivedMessage(
-        event: "portal_signatureRejected",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: "",
-          transactionId: id
-        )
-      )
-
+    Task {
       do {
-        let message = try JSONEncoder().encode(event)
-        self.client?.send(message)
-      } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
-      }
-    }
-
-    let newChainId = chainId ?? self.chainId // use the default chain when there is no chainId sent from Wallet Connect
-    self.handleProviderRequest(method: method, params: params, chainId: newChainId) { [weak self] result in
-      guard let self = self else { return }
-
-      if result.error != nil {
-        print("[PortalConnect] \(result.error!)")
-        return
-      }
-
-      let txHash = result.data as! String
-      let event = SignatureReceivedMessage(
-        event: "signatureReceived",
-        data: SignatureReceivedData(
-          topic: topic,
-          transactionHash: txHash,
-          transactionId: id
+        let (id, method, params, topic, chainId) = (
+          data.id,
+          data.params.request.method,
+          data.params.request.params,
+          data.topic,
+          data.params.chainId
         )
-      )
 
-      do {
+        print("⚠️ Method: \(method), Params: \(params)")
+
+        on(event: Events.PortalSigningRejected.rawValue) { [weak self] _ in
+          guard let self = self else { return }
+          let event = SignatureReceivedMessage(
+            event: "portal_signatureRejected",
+            data: SignatureReceivedData(
+              topic: topic,
+              transactionHash: "",
+              transactionId: id
+            )
+          )
+          do {
+            let message = try JSONEncoder().encode(event)
+            self.client?.send(message)
+          } catch {
+            self.logger.error("⚠️ PortalConnect.on(portal_signingRejected) -  Error encoding SignatureReceivedMessage: \(error)")
+          }
+        }
+
+        // use the default chain when there is no chainId sent from Wallet Connect
+        let newChainId = chainId ?? self.chainId ?? 11_155_111
+        let response = try await self.handleProviderRequest(method: method, params: params, chainId: newChainId)
+
+        guard let transactionHash = response as? String else {
+          throw PortalConnectError.unableToParseSignResponse
+        }
+
+        let event = SignatureReceivedMessage(
+          event: "signatureReceived",
+          data: SignatureReceivedData(
+            topic: topic,
+            transactionHash: transactionHash,
+            transactionId: id
+          )
+        )
+
         let message = try JSONEncoder().encode(event)
-
         self.client?.send(message)
 
         // emit the PortalSignatureReceived event on the PortalConnect EventBus as a convenience
         if signMethods.contains(method) {
-          self.emit(event: Events.PortalSignatureReceived.rawValue, data: result.data!)
+          self.emit(event: Events.PortalSignatureReceived.rawValue, data: transactionHash)
         }
       } catch {
-        print("[PortalConnect] Error encoding SignatureReceivedMessage: \(error)")
+        self.logger.error("⚠️ Error processing session request: \(error)")
       }
     }
   }
@@ -490,30 +520,26 @@ public class PortalConnect: EventBus {
     self.client?.on("session_request_transaction", self.handleSessionRequestTransaction)
   }
 
-  private func handleProviderRequest(method: String, params: [ETHAddressParam], chainId: Int, completion: @escaping (Result<Any>) -> Void) {
-    self.provider.request(payload: ETHAddressPayload(method: method, params: params, chainId: chainId), connect: self) { result in
-      guard result.error == nil else {
-        return completion(Result(error: result.error!))
-      }
-      completion(Result(data: result.data!))
+  private func handleProviderRequest(method: String, params: [Any], chainId: Int) async throws -> Any {
+    let encodedParams = try params.map { param in
+      try AnyEncodable(param)
     }
-  }
+    guard let method = PortalRequestMethod(rawValue: method) else {
+      throw PortalConnectError.unsupportedRequestMethod(method)
+    }
+    let response = try await self.provider.request(
+      "eip155:\(chainId)",
+      withMethod: method,
+      andParams: encodedParams,
+      connect: self
+    )
 
-  private func handleProviderRequest(method: String, params: [ETHTransactionParam], chainId: Int, completion: @escaping (Result<Any>) -> Void) {
-    self.provider.request(payload: ETHTransactionPayload(method: method, params: params, chainId: chainId), connect: self) { (result: Result<TransactionCompletionResult>) in
-      guard result.error == nil else {
-        return completion(Result(error: result.error!))
-      }
-      completion(Result(data: (result.data!.result as! Result<Any>).data as! String))
-    }
+    return response.result
   }
+}
 
-  private func handleProviderRequest(method: String, params: [Any], chainId: Int, completion: @escaping (Result<Any>) -> Void) {
-    self.provider.request(payload: ETHRequestPayload(method: method, params: params, chainId: chainId), connect: self) { result in
-      guard result.error == nil else {
-        return completion(Result(error: result.error!))
-      }
-      completion(Result(data: result.data!))
-    }
-  }
+enum PortalConnectError: Error {
+  case noWebSocketClientFound
+  case unableToParseSignResponse
+  case unsupportedRequestMethod(String)
 }
