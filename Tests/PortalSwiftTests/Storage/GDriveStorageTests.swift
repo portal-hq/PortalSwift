6032a51094ff9d63a7e20f48e5efcd15866decab
Prakash <pksmile.katwal@gmail.com>
2024-06-08 00:34:23 +0200

    set gdrivetest to previous step

--- Tests/PortalSwiftTests/Storage/GDriveStorageTests.swift
+++ Tests/PortalSwiftTests/Storage/GDriveStorageTests.swift
@@ -10,25 +10,7 @@
 import XCTest
 
 final class GDriveStorageTests: XCTestCase {
-  //  func testExample() {
-  //    XCTAssertTrue(true)
-  //  }
-  
-  var storage: Storage?
-  
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    self.storage = Storage()
-  }
-  
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    self.storage = nil
-  }
-  
-  func testDelete() {
-    self.storage!.delete { result in
-      XCTAssert(result.error != nil)
-    }
+  func testExample() {
+    XCTAssertTrue(true)
   }
 }
