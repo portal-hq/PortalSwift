c41627cd7987efbc9ac130a862653178bb8164b7
Rami Shahatit <rami@portalhq.io>
2024-08-05 18:19:51 -0400

    Update Sources/PortalSwift/Core/Mpc/PortalMpc.swift
    Co-authored-by: Ahmed Ragab Issa <AhmedRagabIssa@gmail.com>

--- Sources/PortalSwift/Core/Mpc/PortalMpc.swift
+++ Sources/PortalSwift/Core/Mpc/PortalMpc.swift
@@ -495,7 +495,10 @@ public class PortalMpc {
             let backupResponse = try await parseRecoveryInput(data: decryptedData)
 
             usingProgressCallback?(MpcStatus(status: .generatingShare, done: false))
-            var recoverResponse: PortalMpcGenerateResponse = try await keychain.getShares() ?? [:]
+            var recoverResponse: PortalMpcGenerateResponse = [:]
+            if let shares = try? await keychain.getShares() {
+              recoverResponse = shares
+            }
             if let ed25519Share = backupResponse[PortalCurve.ED25519.rawValue] {
               //  The share's already been backed up, recover it
               async let ed25519MpcShare = try recoverSigningShare(.ED25519, withMethod: method, andBackupShare: ed25519Share.share)
