78eed5284fa21f84bc0ae3cea92a85dd0cbb609b
Ahmed Ragab Issa <AhmedRagabIssa@gmail.com>
2024-08-29 01:18:16 +0300

    [Tests] refactor the `PortalApiTests` for proper tearDown

--- Tests/PortalSwiftTests/Core/PortalApiTests.swift
+++ Tests/PortalSwiftTests/Core/PortalApiTests.swift
@@ -10,18 +10,20 @@
 import XCTest
 
 final class PortalApiTests: XCTestCase {
-  var api: PortalApi = .init(apiKey: MockConstants.mockApiKey, apiHost: MockConstants.mockHost, requests: MockPortalRequests())
+  var api: PortalApi? = .init(apiKey: MockConstants.mockApiKey, apiHost: MockConstants.mockHost, requests: MockPortalRequests())
 
   override func setUpWithError() throws {
     self.api = PortalApi(apiKey: MockConstants.mockApiKey, apiHost: MockConstants.mockHost, requests: MockPortalRequests())
   }
 
-  override func tearDownWithError() throws {}
+  override func tearDownWithError() throws {
+      api = nil
+  }
 
   func testEject() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.eject()")
-    let ejectResponse = try await api.eject()
-    print("⚠️ Eject response:", ejectResponse)
+      let ejectResponse = try await api?.eject()
+    print("⚠️ Eject response:", ejectResponse ?? "")
     XCTAssertEqual(ejectResponse, MockConstants.mockEjectResponse)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
@@ -30,9 +32,9 @@ final class PortalApiTests: XCTestCase {
   func testGetClient() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.getClient()")
     let mockClientResponse = MockConstants.mockClient
-    let clientResponse = try await api.getClient()
-    XCTAssert(!clientResponse.id.isEmpty)
-    XCTAssert(clientResponse.id == mockClientResponse.id)
+    let clientResponse = try await api?.getClient()
+      XCTAssert(!(clientResponse?.id.isEmpty ?? false))
+    XCTAssert(clientResponse?.id == mockClientResponse.id)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
@@ -40,7 +42,7 @@ final class PortalApiTests: XCTestCase {
   func testGetClientCompletion() throws {
     let expectation = XCTestExpectation(description: "PortalApi.getClient(completion)")
     let mockClientResponse = MockConstants.mockClient
-    try self.api.getClient { result in
+      try self.api?.getClient { result in
       XCTAssert(result.data?.id == mockClientResponse.id)
       expectation.fulfill()
     }
@@ -49,7 +51,7 @@ final class PortalApiTests: XCTestCase {
 
   func testGetBackupShareMetadataCompletion() throws {
     let expectation = XCTestExpectation(description: "PortalApi.getBackupShareMetadata(completion)")
-    try api.getBackupShareMetadata { result in
+      try api?.getBackupShareMetadata { result in
       XCTAssert(result.data?.count ?? 0 > 0)
       XCTAssert(result.data?[0].id == MockConstants.mockFetchedShairPair.id)
       expectation.fulfill()
@@ -59,9 +61,9 @@ final class PortalApiTests: XCTestCase {
 
   func testGetBackupSharePairs() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.getSharePairs(.backup)")
-    let backupSharesResponse = try await api.getSharePairs(.backup, walletId: MockConstants.mockWalletId)
-    XCTAssert(backupSharesResponse.count > 0)
-    XCTAssert(backupSharesResponse[0].id == MockConstants.mockFetchedShairPair.id)
+      let backupSharesResponse = try await api?.getSharePairs(.backup, walletId: MockConstants.mockWalletId)
+      XCTAssert(backupSharesResponse?.count ?? 0 > 0)
+      XCTAssert(backupSharesResponse?[0].id == MockConstants.mockFetchedShairPair.id)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
@@ -69,17 +71,17 @@ final class PortalApiTests: XCTestCase {
   func testGetBalances() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.getSharePairs(.backup)")
     let mockFetchedBalance = MockConstants.mockedFetchedBalance
-    let balancesResponse = try await api.getBalances("eip155:11155111")
-    XCTAssert(balancesResponse.count > 0)
-    XCTAssert(balancesResponse[0].contractAddress == mockFetchedBalance.contractAddress)
-    XCTAssert(balancesResponse[0].balance == mockFetchedBalance.balance)
+      let balancesResponse = try await api?.getBalances("eip155:11155111")
+      XCTAssert(balancesResponse?.count ?? 0 > 0)
+      XCTAssert(balancesResponse?[0].contractAddress == mockFetchedBalance.contractAddress)
+      XCTAssert(balancesResponse?[0].balance == mockFetchedBalance.balance)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
 
   func testGetSigningShareMetadataCompletion() throws {
     let expectation = XCTestExpectation(description: "PortalApi.getSigningShareMetadata(completion)")
-    try api.getSigningShareMetadata { result in
+      try api?.getSigningShareMetadata { result in
       XCTAssert(result.data?.count ?? 0 > 0)
       XCTAssert(result.data?[0].id == MockConstants.mockFetchedShairPair.id)
       expectation.fulfill()
@@ -90,9 +92,9 @@ final class PortalApiTests: XCTestCase {
   func testGetSigningSharePairs() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.getSharePairs(.signing)")
     let mockSharesResponse = [MockConstants.mockFetchedShairPair]
-    let signingSharesResponse = try await api.getSharePairs(.signing, walletId: MockConstants.mockWalletId)
+      let signingSharesResponse = try await api?.getSharePairs(.signing, walletId: MockConstants.mockWalletId)
     XCTAssert(signingSharesResponse == mockSharesResponse)
-    XCTAssert(signingSharesResponse[0].id == MockConstants.mockFetchedShairPair.id)
+      XCTAssert(signingSharesResponse?[0].id == MockConstants.mockFetchedShairPair.id)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
@@ -100,8 +102,8 @@ final class PortalApiTests: XCTestCase {
   func testSimulateTransaction() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.simulateTransaction()")
     let transaction = AnyEncodable([:] as [String: String])
-    let simulatedTransaction = try await api.simulateTransaction(transaction, withChainId: "eip155:11155111")
-    XCTAssert(simulatedTransaction.changes.count == MockConstants.mockSimulatedTransaction.changes.count)
+      let simulatedTransaction = try await api?.simulateTransaction(transaction, withChainId: "eip155:11155111")
+      XCTAssert(simulatedTransaction?.changes.count == MockConstants.mockSimulatedTransaction.changes.count)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
@@ -109,10 +111,10 @@ final class PortalApiTests: XCTestCase {
   func testGetTransactions() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.getTransactions()")
     let mockFetchedTransaction = MockConstants.mockFetchedTransaction
-    let mockTransactionsResponse = try await api.getTransactions("eip155:11155111")
-    XCTAssert(mockTransactionsResponse.count > 0)
-    XCTAssert(mockTransactionsResponse[0].blockNum == mockFetchedTransaction.blockNum)
-    XCTAssert(mockTransactionsResponse[0].chainId == mockFetchedTransaction.chainId)
+      let mockTransactionsResponse = try await api?.getTransactions("eip155:11155111")
+      XCTAssert(mockTransactionsResponse?.count ?? 0 > 0)
+      XCTAssert(mockTransactionsResponse?[0].blockNum == mockFetchedTransaction.blockNum)
+      XCTAssert(mockTransactionsResponse?[0].chainId == mockFetchedTransaction.chainId)
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
@@ -120,7 +122,7 @@ final class PortalApiTests: XCTestCase {
   func getGetTransactionsCompletion() throws {
     let expectation = XCTestExpectation(description: "PortalApi.getTransactions(completion)")
     let mockFetchedTransaction = MockConstants.mockFetchedTransaction
-    try self.api.getTransactions { result in
+      try self.api?.getTransactions { result in
       XCTAssert(result.data?.count ?? 0 > 0)
       XCTAssert(result.data?[0].blockNum == mockFetchedTransaction.blockNum)
       XCTAssert(result.data?[0].chainId == mockFetchedTransaction.chainId)
@@ -131,15 +133,15 @@ final class PortalApiTests: XCTestCase {
 
   func testUpdateBackupShareStatus() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.updateShareStatus(.backup)")
-    try await api.updateShareStatus(.backup, status: .STORED_CLIENT_BACKUP_SHARE_KEY, sharePairIds: [MockConstants.mockMpcShareId])
-    try await self.api.updateShareStatus(.backup, status: .STORED_CLIENT_BACKUP_SHARE, sharePairIds: [MockConstants.mockMpcShareId])
+      try await api?.updateShareStatus(.backup, status: .STORED_CLIENT_BACKUP_SHARE_KEY, sharePairIds: [MockConstants.mockMpcShareId])
+      try await self.api?.updateShareStatus(.backup, status: .STORED_CLIENT_BACKUP_SHARE, sharePairIds: [MockConstants.mockMpcShareId])
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
 
   func testUpdateSigningShareStatus() async throws {
     let expectation = XCTestExpectation(description: "PortalApi.updateShareStatus(.signing)")
-    try await api.updateShareStatus(.signing, status: .STORED_CLIENT, sharePairIds: [MockConstants.mockMpcShareId])
+      try await api?.updateShareStatus(.signing, status: .STORED_CLIENT, sharePairIds: [MockConstants.mockMpcShareId])
     expectation.fulfill()
     await fulfillment(of: [expectation], timeout: 5.0)
   }
